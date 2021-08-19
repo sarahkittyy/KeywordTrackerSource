f5392b04fdee9a35a06c4d70bd078e93c918478f
Sarah Ohlin <sarahohlin1@gmail.com>
2021-08-19 01:45:20 -0400

    feat: added inbox to see all missed pings

--- plugins/KeywordTracker/index.js
+++ plugins/KeywordTracker/index.js
@@ -1,11 +1,13 @@
 module.exports = (Plugin, Library) => {
   const switchCss = require('switch.css');
+  const inboxCss = require('inbox.css');
   const defaultSettings = {
     whitelistedUsers: [],
     keywords: [],
     ignoredUsers: [],
     guilds: {},
     enabled: true,
+    unreadMatches: {},
     notifications: true,
   };
   const {
@@ -16,7 +18,9 @@ module.exports = (Plugin, Library) => {
     Settings,
     Utilities,
     PluginUtilities,
+    ReactTools,
     Modals,
+    Tooltip,
     Toasts: Toast,
     DiscordModules: Modules,
   } = Library;
@@ -27,11 +31,24 @@ module.exports = (Plugin, Library) => {
 
   return class KeywordTracker extends Plugin {
     async onStart() {
+      this.cancelPatches = [];
       this.loadSettings();
       PluginUtilities.addStyle(this.getName(), switchCss);
+      PluginUtilities.addStyle(this.getName(), inboxCss);
 
       let dispatchModule = BdApi.findModuleByProps('dispatch');
-      this.cancelPatch = BdApi.monkeyPatch(dispatchModule, 'dispatch', { after: this.handleMessage.bind(this) });
+      this.cancelPatches.push(BdApi.monkeyPatch(dispatchModule, 'dispatch', { after: this.handleMessage.bind(this) }));
+
+      //let navigationModule = Modules.NavigationUtils;
+      //this.cancelPatches.push(BdApi.monkeyPatch(navigationModule, 'transitionTo', { after: () => {
+        //this.buildInboxPanel();
+      //}}));
+      const TitleBar = BdApi.findModuleByProps('Title', 'default', 'Caret');
+      this.cancelPatches.push(BdApi.monkeyPatch(TitleBar, "default", { before: ({ methodArguments: args }) => {
+        let props = args[0];
+        if (props.toolbar.type === 'function') return;
+        props.toolbar.props.children.splice(1, 0, this.buildInboxPanel());
+      } }) );
 
       this.userId = BdApi.findModuleByProps('getId').getId();
     }
@@ -40,7 +57,7 @@ module.exports = (Plugin, Library) => {
       this.saveSettings();
 
       Patcher.unpatchAll(this.getName());
-      this.cancelPatch();
+      this.cancelPatches.forEach(p => p());
       PluginUtilities.removeStyle(this.getName());
     }
 
@@ -125,7 +142,7 @@ module.exports = (Plugin, Library) => {
       }
     }
 
-    sendMatchNotification(thumbnail, title, text, redirect) {
+    sendMatchNotification(thumbnail, title, text, redirect, message) {
       Modules.NotificationModule.showNotification(
         thumbnail,
         title,
@@ -133,6 +150,8 @@ module.exports = (Plugin, Library) => {
         // opts
         {
           onClick: () => {
+            delete this.settings.unreadMatches[message.id];
+            this.saveSettings();
             Modules.NavigationUtils.transitionTo(
               redirect,
               undefined,
@@ -149,11 +168,15 @@ module.exports = (Plugin, Library) => {
         `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.webp?size=256`,
         `User match in ${guild}!`,
         `${message.author.username} typed in #${channel.name}.`,
-        `/channels/${message.guild_id}/${channel.id}/${message.id}`
+        `/channels/${message.guild_id}/${channel.id}/${message.id}`,
+        message,
       );
       if (this.settings.notifications) {
         Modules.SoundModule.playSound("message1", 0.4);
       }
+      message._match = `User ID ${message.author.id}`;
+      this.settings.unreadMatches[message.id] = message;
+      this.saveSettings();
     }
 
     pingSuccess(message, channel, guild, match) {
@@ -162,12 +185,15 @@ module.exports = (Plugin, Library) => {
         `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.webp?size=256`,
         `Keyword match in ${guild}!`,
         `${message.author.username} matched ${match} in #${channel.name}.`,
-        `/channels/${message.guild_id}/${channel.id}/${message.id}`
+        `/channels/${message.guild_id}/${channel.id}/${message.id}`,
+        message,
       );
       if (this.settings.notifications) {
         Modules.SoundModule.playSound("message1", 0.4);
       }
-      //Toast.info(`Message by ${message.author.username} in #${channel.name} matches ${match}`);
+      message._match = `${match}`;
+      this.settings.unreadMatches[message.id] = message;
+      this.saveSettings();
     }
 
     makeSwitch(iv, callback) {
@@ -198,6 +224,146 @@ module.exports = (Plugin, Library) => {
     loadSettings() {
       // load settings
       this.settings = Utilities.deepclone(PluginUtilities.loadSettings('KeywordTracker', defaultSettings));
+    }
+
+    // build the inbox panel placed directly after the pinned messages button
+    buildInboxPanel() {
+      let pinned = document.querySelector('div[aria-label*="Pinned Messages"]');
+      if (!pinned) {
+        return;
+      }
+
+      let inbox = pinned.cloneNode(true);
+      inbox.setAttribute('is-keyword-tracker-inbox', true);
+      inbox.setAttribute('aria-label', 'Keyword Matches');
+      let icon = inbox.querySelector('svg');
+      icon.setAttribute('viewBox', '0 0 20 20');
+      // icon!
+      let iconSVG = `<path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M8.627,7.885C8.499,8.388,7.873,8.101,8.13,8.177L4.12,7.143c-0.218-0.057-0.351-0.28-0.293-0.498c0.057-0.218,0.279-0.351,0.497-0.294l4.011,1.037C8.552,7.444,8.685,7.667,8.627,7.885 M8.334,10.123L4.323,9.086C4.105,9.031,3.883,9.162,3.826,9.38C3.769,9.598,3.901,9.82,4.12,9.877l4.01,1.037c-0.262-0.062,0.373,0.192,0.497-0.294C8.685,10.401,8.552,10.18,8.334,10.123 M7.131,12.507L4.323,11.78c-0.218-0.057-0.44,0.076-0.497,0.295c-0.057,0.218,0.075,0.439,0.293,0.495l2.809,0.726c-0.265-0.062,0.37,0.193,0.495-0.293C7.48,12.784,7.35,12.562,7.131,12.507M18.159,3.677v10.701c0,0.186-0.126,0.348-0.306,0.393l-7.755,1.948c-0.07,0.016-0.134,0.016-0.204,0l-7.748-1.948c-0.179-0.045-0.306-0.207-0.306-0.393V3.677c0-0.267,0.249-0.461,0.509-0.396l7.646,1.921l7.654-1.921C17.91,3.216,18.159,3.41,18.159,3.677 M9.589,5.939L2.656,4.203v9.857l6.933,1.737V5.939z M17.344,4.203l-6.939,1.736v9.859l6.939-1.737V4.203z M16.168,6.645c-0.058-0.218-0.279-0.351-0.498-0.294l-4.011,1.037c-0.218,0.057-0.351,0.28-0.293,0.498c0.128,0.503,0.755,0.216,0.498,0.292l4.009-1.034C16.092,7.085,16.225,6.863,16.168,6.645 M16.168,9.38c-0.058-0.218-0.279-0.349-0.498-0.294l-4.011,1.036c-0.218,0.057-0.351,0.279-0.293,0.498c0.124,0.486,0.759,0.232,0.498,0.294l4.009-1.037C16.092,9.82,16.225,9.598,16.168,9.38 M14.963,12.385c-0.055-0.219-0.276-0.35-0.495-0.294l-2.809,0.726c-0.218,0.056-0.351,0.279-0.293,0.496c0.127,0.506,0.755,0.218,0.498,0.293l2.807-0.723C14.89,12.825,15.021,12.603,14.963,12.385"></path>`;
+      icon.innerHTML = iconSVG;
+      inbox.appendChild(icon);
+
+      // add hover tooltip
+      let tooltip = new Tooltip(inbox, 'Keyword Matches');
+
+      // actual modal window on-click
+      const openModal = () => {
+        Modals.showModal('Keyword Matches', this.renderInbox(), {
+          confirmText: 'Close',
+          cancelText: 'Mark as Read',
+          onCancel: () => {
+            this.settings.unreadMatches = {};
+            this.saveSettings();
+          },
+          onConfirm: () => {
+            this.saveSettings();
+          }
+        });
+      };
+      inbox.removeEventListener('click', openModal);
+      inbox.addEventListener('click', openModal);
+
+      return ReactTools.createWrappedElement(inbox);
+    }
+
+    // render all messages from settings.unreadMatches
+    renderInbox() {
+      let root = document.createElement('div');
+      root.className = 'kt-inbox-container';
+
+      const EntryFlushEvent = new Event('entryflush');
+
+      const setupEntries = () => {
+        let sortedMatches = Object.values(this.settings.unreadMatches)
+          .sort((a, b) => {
+            return new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime();
+          })
+          .filter(msg => { // filter messages older than 60 days
+            let timeDiff = Math.abs(new Date(msg.timestamp).getTime() - new Date().getTime());
+            let daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            return daysDiff <= 60;
+          });
+
+        const matchEntry = (msg) => {
+          let entry = document.createElement('div');
+          entry.className = 'kt-inbox-entry';
+
+          let timestamp = document.createElement('span');
+          timestamp.className = 'timestamp';
+          timestamp.innerHTML = `${new Date(msg.timestamp).toLocaleString()}`;
+          entry.appendChild(timestamp);
+
+          let icon = document.createElement('img');
+          let iconUrl = `https://cdn.discordapp.com/avatars/${msg.author.id}/${msg.author.avatar}.webp?size=256`
+          icon.className = 'usericon';
+          icon.setAttribute('src', iconUrl);
+          entry.appendChild(icon);
+
+          let username = document.createElement('span');
+          username.className = 'username';
+          username.innerHTML = `${msg.author.username}: `;
+          entry.appendChild(username);
+          
+          let content = document.createElement('span');
+          content.className = 'content';
+          content.innerHTML = msg.content;
+          entry.appendChild(content);
+
+          entry.appendChild(document.createElement('br'));
+
+          let matched = document.createElement('span');
+          matched.className = 'matched';
+          matched.innerHTML = `Matched ${msg._match}`;
+          entry.appendChild(matched);
+
+          let markRead = document.createElement('button');
+          markRead.addEventListener('click', () => {
+            delete this.settings.unreadMatches[msg.id];
+            this.saveSettings();
+            root.dispatchEvent(EntryFlushEvent);
+          });
+          markRead.innerHTML = 'Mark as Read';
+          entry.appendChild(markRead);
+
+          let jump = document.createElement('button');
+          jump.addEventListener('click', () => {
+            delete this.settings.unreadMatches[msg.id];
+            this.saveSettings();
+            let modal = document.querySelector('div[class*="layerContainer"] > div[class*="backdrop"][class*="withLayer"]');
+            if(modal) {
+              let parent = modal.parentNode;
+              while (parent.hasChildNodes()) {
+                  parent.removeChild(parent.lastChild);
+              }
+            }
+            Modules.NavigationUtils.transitionTo(
+              `/channels/${msg.guild_id}/${msg.channel_id}/${msg.id}`,
+              undefined,
+              undefined,
+            );
+          });
+          jump.innerHTML = 'Jump';
+          entry.appendChild(jump);
+
+          return entry;
+        };
+        if (sortedMatches.length === 0) {
+          root.innerHTML = 'No recent matches.';
+          root.setAttribute('style', 'line-height: 90px; text-align: center;');
+        } else {
+          for(let msg of sortedMatches) {
+            root.appendChild(matchEntry(msg));
+          }
+        }
+      };
+      setupEntries();
+
+      root.addEventListener('entryflush', () => {
+        root.innerHTML = '';
+        setupEntries();
+      });
+
+      return ReactTools.createWrappedElement(root);
     }
 
     //TODO: god why
